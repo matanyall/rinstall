776310cbb658298f0d394db326eac4dba5b2a5bf
Matanya Loewenthal <matanya@loewenthal.net>
2022-01-19 09:23:24 -0700

    feat(cli): structured subcommands so they work now

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ fn main() {
     let m = App::new("rinstall")
         .version("0.1.0")
         .author("Matanya Loewenthal");
+        // .arg(Arg::new("INPUT")
+        //         .required(true)
+        //         .help("Apply this input file to your system")
+        //         .index(1));
     // let m = m.arg(Arg::new("config")
     //     .short('c')
     //     .long("config")
@@ -22,19 +26,71 @@ fn main() {
     //     .help("Sets a custom config file")
     //     .takes_value(true));
 
-    let m = m.subcommand(App::new("apply")
-            .about("Apply the configuration file")
-            .arg(Arg::new("config")
-                .short('c')
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .takes_value(true))
-            .setting(AppSettings::ArgRequiredElseHelp));
+    let apply = App::new("apply")
+        .version("0.1.0")
+        .author("Matanya Loewenthal")
+        .about("Apply this input file to your system")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::new("INPUT")
+                .required(true)
+                .help("Apply this input file to your system")
+                .index(1));
+    
+    let capture = App::new("capture")
+        .version("0.1.0")
+        .author("Matanya Loewenthal")
+        .about("Capture your system configuration")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::new("OUTPUT")
+                .required(true)
+                .help("Capture your system configuration")
+                .index(1));
+
+    let m = m.subcommand(apply);
+    let m = m.subcommand(capture);
+            
 
     let matches = m.get_matches();
+    match matches.subcommand() {
+        Some(("apply", matches)) => {
+            let input_file = matches.value_of("INPUT").unwrap();
+            let mut f = File::open(input_file).expect("file not found");
+            let mut contents = String::new();
+            f.read_to_string(&mut contents).expect("something went wrong reading the file");
+            println!("input file is:\n{}", contents);
+            // let yaml = YamlLoader::load_from_str(&contents).unwrap();
+            // let yaml = yaml[0].clone();
+            // let yaml = yaml.as_hash().unwrap();
+            // let yaml = yaml.clone();
+            // let yaml = yaml.get("apt").unwrap();
+            // let yaml = yaml.as_hash().unwrap();
+            // let yaml = yaml.clone();
+            // apt_packages::install_packages(&yaml);
+            // let yaml = yaml.get("snap").unwrap();
+            // let yaml = yaml.as_hash().unwrap();
+            // let yaml = yaml.clone();
+            // snap_packages::install_packages(&yaml);
+        },
+        Some(("capture", matches)) => {
+            // let output_file = matches.value_of("OUTPUT").unwrap();
+            // let mut f = File::create(output_file).expect("file not found");
+            // let yaml = YamlLoader::load_from_str("").unwrap();
+            // let yaml = yaml[0].clone();
+            // let yaml = yaml.as_hash().unwrap();
+            // let yaml = yaml.clone();
+            // let yaml = yaml.get("apt").unwrap();
+            // let yaml = yaml.as_hash().unwrap();
+            // let yaml = yaml.clone();
+            // apt_packages::install_packages(&yaml);
+            // let yaml = yaml.get("snap").unwrap();
+            // let yaml = yaml.as_hash().unwrap();
+            // let yaml = yaml.clone();
+            // snap_packages::install_packages(&yaml);
+        },
+        _ => {}
+    }
 
-    println!("{:?}", matches);
+    println!("{:?}", matches.value_of("INPUT"));
 
 
     println!("Hello, world!");
@@ -63,4 +119,13 @@ fn load_file(file: &str) -> Vec<Yaml> {
     docs
 
     // iterate / process doc[s] ..
+}
+
+struct Config {
+    input_file: String,
+    output_file: String,
+}
+
+struct CLI {
+    config: Config,
 }
