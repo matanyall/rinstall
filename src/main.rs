abc175a8bb43d95ef9b7bb735e3943150ca5c2a8
Matanya Loewenthal <matanya@loewenthal.net>
2022-01-18 20:34:27 -0700

    feat(cli): started clap interface for apply subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::process::Command;
 
 extern crate yaml_rust;
 use yaml_rust::{Yaml};
 use yaml_rust::YamlLoader;
 
+
+use clap::{App, Arg, arg, AppSettings};
+
+
+use rinstall::{apt_packages, snap_packages};
+
 fn main() {
+    let m = App::new("rinstall")
+        .version("0.1.0")
+        .author("Matanya Loewenthal");
+    // let m = m.arg(Arg::new("config")
+    //     .short('c')
+    //     .long("config")
+    //     .value_name("FILE")
+    //     .help("Sets a custom config file")
+    //     .takes_value(true));
+
+    let m = m.subcommand(App::new("apply")
+            .about("Apply the configuration file")
+            .arg(Arg::new("config")
+                .short('c')
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true))
+            .setting(AppSettings::ArgRequiredElseHelp));
+
+    let matches = m.get_matches();
+
+    println!("{:?}", matches);
+
+
     println!("Hello, world!");
     // put in help menu and CLI options
 
@@ -14,18 +44,10 @@ fn main() {
     // read in the yaml file
     let file_name = ".test.yml";
     let docs = load_file(file_name);
-    let apt_package_list = docs[0]["packages"]["apt"]["utilities"].as_vec().unwrap();
-    let apt_package_list = apt_package_list.iter().map(|x| x.as_str().unwrap()).collect::<Vec<&str>>();
-    println!("{:?}", apt_package_list);
 
-    // install the packages
-    let output = Command::new("apt")
-        .arg("install")
-        .args(apt_package_list)
-        .output()
-        .expect("failed to execute process");
-    
-    println!("{}", String::from_utf8_lossy(&output.stderr));
+
+    apt_packages::install_packages(&docs[0]["packages"]["apt"]);
+    snap_packages::install_packages(&docs[0]["packages"]["snap"]);
 
 }
 
